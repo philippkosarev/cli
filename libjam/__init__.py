33307a39f1d43a93d78da6c652a0f7dcec53df53
PhilippKosarev <philipp.kosarev@gmail.com>
2026-08-29 12:27:29 +0100

    remove all libjam modules except captain

--- libjam/__init__.py
+++ libjam/__init__.py
@@ -6,8 +6,3 @@ PyPi page: https://pypi.org/project/libjam
 """
 
 from .captain import Captain, captain
-from .secretary import Secretary, File
-from . import writer
-from . import flashcard
-from . import drawer
-from .path import Path
